f924762cdf6ff914117bd7b593deb1c442abbbbe
MateuszM-M <mateuszmikulski2@gmail.com>
2022-05-03 11:58:49 +0200

    Add layout

--- src/courses/views.py
+++ src/courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 
-class Home(View):
+class Dashboard(View):
     def get(self, request):
-        return render(request, 'home.html')
+        return render(request, 'courses/dashboard.html')
 
