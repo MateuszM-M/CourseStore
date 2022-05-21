47fa20015fe20b072dc830c3269786f34ce5fec0
MateuszM-M <mateuszmikulski2@gmail.com>
2022-05-21 19:25:18 +0200

    Add course list for teachers

--- src/courses/views.py
+++ src/courses/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.utils.text import slugify
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView
 from .forms import AddCourseForm
 
 from .models import Course, Module
@@ -12,8 +12,12 @@ class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = 'courses/dashboard.html'
 
 
-class TeacherDashboard(LoginRequiredMixin, TemplateView):
+class TeacherDashboard(LoginRequiredMixin, ListView):
+    model = Course
     template_name = 'courses/teacher/teacher_dashboard.html'
+    
+    def get_queryset(self):
+        return Course.objects.filter(owner=self.request.user)
     
 
 class CreateCourse(LoginRequiredMixin, CreateView):
@@ -26,6 +30,7 @@ class CreateCourse(LoginRequiredMixin, CreateView):
         form.instance.owner = self.request.user
         form.instance.slug = slugify(form.instance.title)
         return super().form_valid(form)
+    
                 
 class CreateModule(LoginRequiredMixin, CreateView):
     model = Module
